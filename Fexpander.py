0e2646453f39c7fa1663030fe09359f760a16ca9
toushangyouxiang <2375741859@qq.com>
2018-05-25 22:23:46 +0800

    Auto compelete added.

--- Fexpander.py
+++ Fexpander.py
@@ -1,15 +1,48 @@
 import os
 from os.path import exists
-storage="/storage/emulated/0/"
-path=input("Please type in the path:\n"+storage)
-path=storage+path
-name=input("Please type in the file name:\n")
-mf=open(path+"/"+name,"rb")
-msize=len(mf.read())/1024/1024
-print("the size is %s"%(msize))
-size=input("Please type in the size you want to reach(MB):\n")
+from dirTest import file_name as fn
 
+def autoCompelete(path,name):
+	files=fn(path)
+	boolean=False
+	index=[]
+	j=0
+	for i in range(len(files)):
+		boolean=name in files[i]
+		if(boolean): 
+			index.append(i)
+			j+=1
+			print(str(j)+":"+files[i]+"  ")
+	a=int(input("--> "))-1
+	return files[index[a]]
 
+#get file's path andi size
+path=input("The path:\n")
+files=fn(path)
+name=input("File name:\n")
+exist=exists(path+"/"+name)
+if(not exist):
+	name=autoCompelete(path,name)
+mfile=open(path+"/"+name,"rb")
+msize=len(mfile.read())/1024/1024
+mfile.close()
+print("The size is %s"%(msize))
+size=input("The size you want to reach(MB):\n")
+
+"""def autoCompelete(path,name):
+	files=fn(path)
+	boolean=False
+	index=[]
+	j=0
+	for i in range(len(files)):
+		boolean=name in files[i]
+		if(boolean):
+			index.append(i)
+			j+=1
+			print(j+":"+files[i]+"  ")
+	a=int(input("--> "))-1
+	return files[index[a]]	
+"""
 
 def expand(fpath,size):
     fpath=str(fpath)
@@ -37,7 +70,7 @@ def expand(fpath,size):
 if(exists(path)):
     print("The path exists.")
     expand((path+"/"+name),size)
-    print("Done.%r MB was written"%size)
+    print("%r MB was written"%size)
 else:
     print("Wrong path!")
 
