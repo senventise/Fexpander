0fd2197c41103964cfeae1dcac8bec6e712aa852
toushangyouxiang <2375741859@qq.com>
2018-05-26 12:05:35 +0800

    Better auto complete and bug fixes

--- Fexpander.py
+++ Fexpander.py
@@ -1,31 +1,24 @@
 import os
+import color
 from os.path import exists
 from dirTest import file_name as fn
 
-def autoCompelete(path,name):
-	files=fn(path)
-	boolean=False
-	index=[]
-	j=0
-	for i in range(len(files)):
-		boolean=name in files[i]
-		if(boolean): 
-			index.append(i)
-			j+=1
-			print(str(j)+":"+files[i]+"  ")
-	a=int(input("--> "))-1
-	return files[index[a]]
+def fileList(path):
+    files=fn(path)
+    for index,item in enumerate(files):
+        print(str(index+1)+"\t"+item)
+    a=int(input("-->"))
+    return files[a-1]
 
-#get file's path andi size
-path=input("The path:\n")
+#get file's path and size
+path=input("The path:")
 files=fn(path)
-name=input("File name:\n")
-exist=exists(path+"/"+name)
-if(not exist):
-	name=autoCompelete(path,name)
+#name=input("File name:")
+name=fileList(path)
 mfile=open(path+"/"+name,"rb")
 msize=len(mfile.read())/1024/1024
 mfile.close()
+print(color.generate("\n\n%s"%name,color.color.YELLOW))
 print("The size is %s"%(msize))
 size=input("The size you want to reach(MB):\n")
 
@@ -46,16 +39,16 @@ def expand(fpath,size):
         file=open(fpath,"ab")
         file.write(temp)
         file.close()
+        print(color.generate("%r MB was written"%size,color.color.GREEN))
     else:
-        print("Wrong input!")
+        print(color.generate("Wrong size!",color.color.RED))
 
 
 
 #main function
 if(exists(path)):
-    print("The path exists.")
+    print(color.generate("The path exists.",color.color.GREEN))
     expand((path+"/"+name),size)
-    print("%r MB was written"%size)
 else:
-    print("Wrong path!")
+    print(color.generate("Wrong path!",color.color.RED))
 
---
+++ color.py
@@ -0,0 +1,20 @@
+class color():
+    BLACK=30
+    RED=31
+    GREEN=32
+    YELLOW=33
+    BLUE=34
+    PINK=35
+    WHITE=37
+    QIN=36
+
+def generate(content,color):
+    content=str(content)
+    color=str(color)
+    START=("\033[0;"+color+";40m")
+    END="\033[0m"
+    OUT=(START+content+END)
+    return OUT
+
+#test
+#print(generate("Hello world!",color.YELLOW))
